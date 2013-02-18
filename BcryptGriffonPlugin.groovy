d3d069db00b13b6c79033378766f0df7c708259b
aalmiray <aalmiray@gmail.com>
2013-02-18 17:53:03 +0100

    first commit

---
+++ BcryptGriffonPlugin.groovy
@@ -0,0 +1,48 @@
+class BcryptGriffonPlugin {
+    // the plugin version
+    String version = '0.1'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '1.2.0 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [:]
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = '<UNKNOWN>'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, qt
+    List toolkits = []
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = ''
+    // Map of Bnd directives and/or Manifest entries
+    // see http://www.aqute.biz/Bnd/Bnd for reference
+    Map manifest = [
+        'Bundle-Description': 'Plugin summary/headline'
+    ]
+
+    List authors = [
+        [
+            name: 'Your Name',
+            email: 'your@email.com'
+        ]
+    ]
+    String title = 'Plugin summary/headline'
+    // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
+    String description = '''
+Brief description of Bcrypt.
+
+Usage
+----
+Lorem ipsum
+
+Configuration
+-------------
+Lorem ipsum
+'''
+}
